3866fb7230f86da8a53fc1664dd9320b1ece401a
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-02-05 21:35:17 +0200

    Dashboard Setup

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
 import { useEffect } from "react";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
@@ -14,10 +13,5 @@ export default function SetupPage() {
         }
     }, [isOpen, onOpen]);
 
-    return (
-        <div className="h-screen p-4">
-            <UserButton afterSignOutUrl="/" />
-            <p>This is a protected route.</p>
-        </div>
-    );
+    return null
 }
